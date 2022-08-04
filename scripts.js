e82dd0a0d0938690cfd328570b717a40c7ed8919
Lucas <lucascraveiropaes@gmail.com>
2022-08-03 21:25:24 -0300

    Arquivos iniciais

---
+++ scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  const button = document.querySelector("button.toggle-mode");
+
+  button.addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+  });
+});
